464a4ee0cf57db04063c3a476d20076f474ee4f0
mohsmadi <msmadi_7@yahoo.com>
2021-03-20 16:05:19 +0200

    last update

--- js/app.js
+++ js/app.js
@@ -1,98 +1,272 @@
-
 'use strict';
 
-const allProudct = [
 
+const allProducts = [];
+
+
+function Product(name) {
+  this.name = name;
+  this.path = `img/${name}`;
+  this.timesClicked = 0;
+  this.timesDisplayed = 0; 
+}
+
+
+const UiController = {};
+
+
+UiController.NUMBER_OF_PRODUCTS_TO_DISPLAY = 3;
+
+
+UiController.ulEl = document.getElementById('product-images');
+UiController.spanEl = document.getElementById('votes');
+UiController.graphOfProductVoteCounts = document.getElementById('graph-content');
+UiController.previousProductsShown = [];
+UiController.currentProductsShown = [];
+UiController.productsVoteCounts = [];
+UiController.MAX_VOTES = 25;
+UiController.productNames = [
   'ball.jpg',
-  'basketball.gif',
-  'chessA.jpeg',
-  'chessb.jpg',
-  'football.jpg',
-  'g-class.webp',
-  'handball.jpg',
-  'iphone_11.gif',
-  'iphone.jpg',
-  'new-product.jpg',
-  'pianoA.jpg',
-  'realmadrid.jpg',
-  'rulex-az-stowe.jpg',
-  'rulexblack.jpg',
-  'shose.jpg',
-  'sun-class.jpg',
-  'sunclass.jpg',
-  'Versace-dylanblue.jpg',
-  'Versace-eros.jpg',
-  'piano.webp',
-]
-// console.log(allProudct);
+'basketball.gif',
+'chessA.jpeg',
+'chessb.jpg',
+'football.jpg',
+'g-class.webp',
+'handball.jpg',
+'iphone_11.gif',
+'iphone.jpg',
+'new-product.jpg',
+'pianoA.jpg',
+'realmadrid.jpg',
+'rulex-az-stowe.jpg',
+'rulexblack.jpg',
+'shose.jpg',
+'sun-class.jpg',
+'sunclass.jpg',
+'Versace-dylanblue.jpg',
+'Versace-eros.jpg',
+'piano.webp',];
 
-const proudctDOM = [
-  document.getElementById('proudctSection'),
-  document.getElementById('liftProudct'), 
-  document.getElementById('middelProudct'), 
-  document.getElementById('rightProudct'), 
-];
 
-// console.table(proudctDOM);
-// const imgPath = ['jpg', 'webp', 'gif'];
- function catalog(imgName){
-    this.name = imgName;
-    this.imgPath= `./img/${imgName}`
-    this.seen = 0;
-    this.voits=0;
-    catalog.full.push(this);
- }
-    catalog.full=[];
+UiController.getUniqueRandomNumbers = function() {
+  const randomNumer;
+  UiController.previousProductsShown = UiController.currentProductsShown;
+  UiController.currentProductsShown = [];
 
-    for (let i = 0; i< allProudct.length; i++){
-         new catalog(allProudct[i]);
+
+  while (UiController.currentProductsShown.length < UiController.NUMBER_OF_PRODUCTS_TO_DISPLAY) {
+    randomNumer = getRandomNumber();
+    if (UiController.currentProductsShown.indexOf(randomNumer) === -1 &&
+    UiController.previousProductsShown.indexOf(randomNumer) === -1) {
+      UiController.currentProductsShown.push(randomNumer);
     }
-
-    function render(){
-      console.log(catalog.full);
-
-     const proudctInTheLift= randomNumber (0,catalog.full.length-1);
-     const leftProudctCatalog=catalog.full[proudctInTheLift];
-     proudctDOM[1].src = leftProudctCatalog.imgPath;
-     proudctDOM[1].alt = leftProudctCatalog.imgName;
-     proudctDOM[1].title = leftProudctCatalog.imgName;
-
-     console.log(proudctInTheLift);
-
-     const proudctInTheMiddle = randomNumber (0,catalog.full.length-1);
-     const middleProudctCatalog = catalog.full[proudctInTheMiddle];
-     proudctDOM[2].src = middleProudctCatalog.imgPath;
-     proudctDOM[2].alt = middleProudctCatalog.imgName;
-     proudctDOM[2].title = middleProudctCatalog.imgName;
-
-     const proudctInTheRight= randomNumber (0,catalog.full.length-1);
-     const rightProudctCatalog = catalog.full[proudctInTheRight];
-     proudctDOM[3].src =  rightProudctCatalog.imgPath;
-     proudctDOM[3].alt =  rightProudctCatalog.imgName;
-     proudctDOM[3].title =  rightProudctCatalog.imgName;
-
-    }
-    render();
-
-    proudctDOM[0].addEventListener('click', checkClick);
-
-    function checkClick(event){
-      if (event.target.id === 'liftProudct' ||event.target.id === 'middelProudct' || event.target.id === 'rightProudct'){
-      // if (event.target.id !== 'proudctSection'){
-        for (let i = 0; i < catalog.full.length; i++);
-        if (catalog.full[i] === event.target.title );{
-          catalog.full[i].seen++
-          catalog.full[i].voits++
-        }
-      }
-
-      render();
-    }
+  }
+};
 
 
-    function randomNumber(min, max) {
-      
-     return Math.floor(Math.random() * (max - min + 1)) + min;
+UiController.renderProducts = function() {
+  UiController.getUniqueRandomNumbers();
+  UiController.ulEl.innerHTML = '';
+
+  const LI = 'li';
+  const FIGURE = 'figure';
+  const IMG = 'img';
+  const FIGCAPTION = 'figcaption';
+
+  for (const i = 0; i < UiController.NUMBER_OF_PRODUCTS_TO_DISPLAY; i++) {
+    const ilEl = document.createElement(LI);
+    const figureEl = document.createElement(FIGURE);
+    const imgEl = document.createElement(IMG);
+    imgEl.src = allProducts[UiController.currentProductsShown[i]].path;
+    imgEl.alt = allProducts[UiController.currentProductsShown[i]].name;
+    const figCaptionEl = document.createElement(FIGCAPTION);
+    figCaptionEl.textContent = allProducts[UiController.currentProductsShown[i]].name;
+
+    UiController.ulEl.appendChild(ilEl);
+    ilEl.appendChild(figureEl);
+    figureEl.appendChild(imgEl);
+    figureEl.appendChild(figCaptionEl);
+
+    allProducts[UiController.currentProductsShown[i]].timesDisplayed++;
+  }
+};
+
+
+UiController.clickedOn = function(event) {
+  const elementClickedOn = event.target.textContent;
+  if (!elementClickedOn) {
+    elementClickedOn = event.target.alt;
   }
 
-  render();
+  
+  const objectToUpdate = allProducts.filter(function(object) {
+    return object.name === elementClickedOn;
+  });
+
+  objectToUpdate[0].timesClicked++;
+
+  UiController.updateTimesClickedToLocalStorage();
+  UiController.checkIfFinishedVoting();
+};
+
+
+UiController.gatherProductsVoteCounts = function() {
+  UiController.productsVoteCounts = [];
+  allProducts.forEach(function(product) {
+    UiController.productsVoteCounts.push(product.timesClicked);
+  });
+};
+
+
+UiController.changeElementStyles = function() {
+  const instructionPEl = document.getElementById('instructions-to-user');
+  const resultsPEl = document.getElementById('results-graph-label');
+
+  UiController.graphOfProductVoteCounts.style.display = 'block';
+  instructionPEl.style.display = 'none';
+  resultsPEl.style.display = 'block';
+};
+
+
+UiController.updateTimesClickedToLocalStorage = function() {
+  UiController.gatherProductsVoteCounts();
+  localStorage.setItem('voteCounts', JSON.stringify(UiController.productsVoteCounts));
+};
+
+
+UiController.updateVoteCountsWithLocalStorage = function() {
+  const storedProductVoteCounts =  JSON.parse(localStorage.getItem('voteCounts'));
+  if (storedProductVoteCounts !== null && storedProductVoteCounts.length > 0) {
+    for (const i = 0; i < allProducts.length; i++) {
+      allProducts[i].timesClicked = storedProductVoteCounts[i];
+    }
+  }
+};
+
+
+
+UiController.checkIfFinishedVoting = function() {
+  const totalUserClicks = UiController.productsVoteCounts.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+  });
+
+  if (totalUserClicks === UiController.MAX_VOTES) {
+    UiController.ulEl.removeEventListener('click', UiController.clickedOn);
+    UiController.ulEl.innerHTML = '';
+    
+    UiController.changeElementStyles();
+    drawGraphOfProductsVoteCounts();
+    UiController.productsVoteCounts = [];
+    localStorage.setItem('voteCounts', JSON.stringify(UiController.productsVoteCounts));
+  } else {
+    UiController.renderProducts();
+  }
+};
+
+
+const getRandomNumber = function() {
+  return Math.floor(Math.random() * UiController.productNames.length);
+};
+
+
+(function() {
+  
+  UiController.productNames.forEach(function(product) {
+    allProducts.push(new Product(product));
+  });
+
+  UiController.spanEl.textContent = UiController.MAX_VOTES;
+
+  
+  UiController.ulEl.addEventListener('click', UiController.clickedOn);
+
+  UiController.renderProducts();
+  UiController.updateVoteCountsWithLocalStorage();
+})();
+
+
+const drawGraphOfProductsVoteCounts = function() {
+  const TWO_D = '2d';
+  const red = 'rgba(255, 99, 132, 0.2)';
+  const blue = 'rgba(54, 162, 235, 0.2)';
+  const yellow = 'rgba(255, 206, 86, 0.2)';
+  const green = 'rgba(75, 192, 192, 0.2)';
+  const purple = 'rgba(153, 102, 255, 0.2)';
+  const redBorder = 'rgba(255, 99, 132, 1)';
+  const blueBorder = 'rgba(54, 162, 235, 1)';
+  const yellowBorder = 'rgba(255, 206, 86, 1)';
+  const greenBorder = 'rgba(75, 192, 192, 1)';
+  const purpleBorder = 'rgba(153, 102, 255, 1)';
+
+  const context = UiController.graphOfProductVoteCounts.getContext(TWO_D);
+
+  new Chart(context, { 
+    type: 'bar',
+    data: {
+      labels: UiController.productNames,
+      responsive: true,
+      datasets: [{
+        label: 'Number of Votes',
+        data: UiController.productsVoteCounts,
+        backgroundColor: [
+          red,
+          blue,
+          yellow,
+          green,
+          purple,
+          red,
+          blue,
+          yellow,
+          green,
+          purple,
+          red,
+          blue,
+          yellow,
+          green,
+          purple,
+          red,
+          blue,
+          yellow,
+          green,
+          purple,
+        ],
+        borderColor: [
+          redBorder,
+          blueBorder,
+          yellowBorder,
+          greenBorder,
+          purpleBorder,
+          redBorder,
+          blueBorder,
+          yellowBorder,
+          greenBorder,
+          purpleBorder,
+          redBorder,
+          blueBorder,
+          yellowBorder,
+          greenBorder,
+          purpleBorder,
+          redBorder,
+          blueBorder,
+          yellowBorder,
+          greenBorder,
+          purpleBorder,
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            stepSize: 1,
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+};
